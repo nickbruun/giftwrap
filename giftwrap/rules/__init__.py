f7f6a567a887e14b50303b709530a75b4345d2ff
Nick Bruun <nick@bruun.co>
2012-08-21 09:46:57 +0200

    Add post-installation script execution rule.

--- giftwrap/rules/__init__.py
+++ giftwrap/rules/__init__.py
@@ -1,3 +1,5 @@
 from runit_rule import RunItRule
 from user_rule import UserRule
 from glob_rule import GlobRule
+from postinst_script_rule import PostInstallScriptRule
+
---
+++ giftwrap/rules/postinst_script_rule.py
@@ -0,0 +1,25 @@
+from base import *
+
+class PostInstallScriptRule(Rule):
+    """Post installation script rule.
+    
+    Allows the execution of one or more shell script lines after installation 
+    has succeeded.
+    """
+    
+    def __init__(self, 
+                 script):
+        """Initialize a post installation script rule.
+        
+        :param script: String or, preferably, list of strings containing the 
+        lines of the script.
+        """
+        
+        self._script = script if isinstance(script, list) else script.split('\n')
+    
+    
+    def apply(self, 
+              package, 
+              context):
+        
+        context.postinst_commands += self._script
