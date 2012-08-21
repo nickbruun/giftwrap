19e35dd83fe9a50524fd700ab8b86e08a7a7f93e
Nick Bruun <nick@bruun.co>
2012-08-21 10:02:10 +0200

    Add log directory rule.

--- giftwrap/rules/__init__.py
+++ giftwrap/rules/__init__.py
@@ -2,4 +2,4 @@ from runit_rule import RunItRule
 from user_rule import UserRule
 from glob_rule import GlobRule
 from postinst_script_rule import PostInstallScriptRule
-
+from log_directory_rule import LogDirectoryRule
---
+++ giftwrap/rules/log_directory_rule.py
@@ -0,0 +1,35 @@
+from base import *
+
+class LogDirectoryRule(Rule):
+    """Log directory rule.
+    
+    :ivar name: Log directory name.
+    :ivar user: default `root`, Username to execute an application as.
+    :ivar group: default ``None``, Group name to execute an application under.
+    """
+    
+    def __init__(self, 
+                 name, 
+                 user = 'root', 
+                 group = None):
+        """Initialize a log directory rule.
+        
+        :param name: Log directory name.
+        :param user: default `root`, Username to execute an application as.
+        :ivar group: default ``None``, Group name to execute an application 
+        under.
+        """
+        
+        self._name = name
+        self._user = user
+        self._group = group
+    
+    
+    def apply(self, 
+              package, 
+              context):
+        # Create the log directory.
+        log_path = '/var/log/%s' % (self._name)
+        context.make_data_dir(log_path, 
+                              user = self._user, 
+                              group = self._group)
